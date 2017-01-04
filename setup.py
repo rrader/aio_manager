2707689735857e210c517e94188d7c53371230e9
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-01-04 13:56:40 +0200

    Upgrade package description
    * Add postgres and mysql extras
    * Bump version
    * Deprecate Python 3.3 support, advertise support for Python 3.6

--- setup.py
+++ setup.py
@@ -5,16 +5,20 @@ classifiers = [
     'License :: OSI Approved :: BSD License',
     'Intended Audience :: Developers',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'Environment :: Web Environment',
     'Development Status :: 4 - Beta',
 ]
 
+extras_require={'sa': ['sqlalchemy>=0.9']}
+extras_require['postgres'] = ['aiopg', *extras_require['sa']]
+extras_require['mysql'] = ['aiomysql', *extras_require['sa']]
+
 
 setup(name='aio_manager',
-      version='0.1.2',
+      version='1.0.0',
       description='Script manager for aiohttp.',
       long_description=('Script manager for aiohttp.\n'
                         'Inspired by Flask-script. Allows to write external scripts. '),
@@ -26,7 +30,7 @@ setup(name='aio_manager',
       license='BSD',
       packages=find_packages(),
       install_requires=['aiohttp', 'colorama'],
-      extras_require={'sa': ['sqlalchemy>=0.9'], },
+      extras_require=extras_require,
       provides=['aio_manager'],
       include_package_data = True,
       test_suite="tests.test_manager")
