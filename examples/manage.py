cc93d6b9ade1d15236904978f012f91b0a9d567d
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-01-04 13:56:10 +0200

    Update sqlalchemy command configuration example

--- examples/manage.py
+++ examples/manage.py
@@ -10,11 +10,14 @@ manager = Manager(app)
 # To support SQLAlchemy commands, use this
 #
 # from aio_manager.commands.ext import sqlalchemy
+# [from aiopg.sa import create_engine]
 # sqlalchemy.configure_manager(manager, app, Base,
 #                              settings.DATABASE_USERNAME,
+#                              settings.DATABASE_PASSWORD,
 #                              settings.DATABASE_NAME,
 #                              settings.DATABASE_HOST,
-#                              settings.DATABASE_PASSWORD)
+#                              settings.DATABASE_PORT[,
+#                              create_engine])
 
 if __name__ == "__main__":
     manager.run()
